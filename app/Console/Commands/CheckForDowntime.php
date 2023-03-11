686ca4e8d98a9c5d15f5f6801d17c34cd44501d7
Mattia Giacobbe <mattia.giacobbe@gmail.com>
2023-03-11 20:48:16 +0100

    feat: add cronjob for disrupted operativity

---
+++ app/Console/Commands/CheckForDowntime.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\OperativityDisrupted;
+use App\Models\Message;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class CheckForDowntime extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'check:downtime';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check if some devices stopped sending activity signals.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $messages = Message::all();
+
+        foreach ($messages as $message) {
+            if ($message->updated_at->diffInMinutes(Carbon::now()) > 10) {
+                Mail::to($message['email'])->send(new OperativityDisrupted());
+            }
+        }
+    }
+}
