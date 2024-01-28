8277b1fe178503a50904ff07fdc632c012e35421
Mattia Giacobbe <mattia.giacobbe@gmail.com>
2024-01-28 17:27:19 +0100

    feat: improve serial number page ux

--- resources/js/Pages/SerialNumbers.jsx
+++ resources/js/Pages/SerialNumbers.jsx
@@ -1,9 +1,10 @@
 import { Head, router } from "@inertiajs/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
+import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import PrimaryButton from "@/Components/PrimaryButton";
-import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 
 export default function SerialNumbers({ auth, serialNumbers }) {
     const createSerialNumber = () => {
@@ -26,29 +27,39 @@ export default function SerialNumbers({ auth, serialNumbers }) {
             <Head title="Codici seriali" />
 
             <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                <div className="grid grid-cols-2 gap-4">
-                    <ul className="w-80 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg ">
-                        {serialNumbers.map((serialNumber) => (
-                            <li
-                                className="flex justify-between w-full px-4 py-2 border-b border-gray-200 rounded-t-lg"
-                                key={serialNumber.id}
-                            >
-                                {serialNumber.code}{" "}
-                                <FontAwesomeIcon
-                                    className="cursor-pointer text-red-700"
-                                    icon={faTrashCan}
-                                    onClick={() =>
-                                        deleteSerialNumber(serialNumber.id)
-                                    }
-                                />
-                            </li>
-                        ))}
-                    </ul>
-
-                    <div>
-                        <PrimaryButton onClick={createSerialNumber}>
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                    <div className="flex flex-col items-start gap-2">
+                        <h1 className="text-2xl">Genera codici seriali</h1>
+                        <p>Clicca per generare un nuovo codice seriale randomico di 8 cifre.</p>
+                        <div className="flex items-center p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-100" role="alert">
+                            <FontAwesomeIcon className="flex-shrink-0 inline w-4 h-4 me-3" icon={faCircleInfo} />
+                            <div>
+                                <span className="font-medium">Attenzione!</span> Durante la registrazione di un nuovo dispositivo verrà restituito un errore qualora il codice seriale inviato non sia presente nella lista.
+                            </div>
+                        </div>
+                        <PrimaryButton className="w-auto" onClick={createSerialNumber}>
                             Genera SN
                         </PrimaryButton>
+                    </div>
+
+                    <div className="flex flex-col justify-end">
+                        <ul className="max-h-80 overflow-y-scroll text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg ">
+                            {serialNumbers.map((serialNumber) => (
+                                <li
+                                    className="flex justify-between w-full px-4 py-2 border-b border-gray-200 rounded-t-lg"
+                                    key={serialNumber.id}
+                                >
+                                    {serialNumber.code}{" "}
+                                    <FontAwesomeIcon
+                                        className="cursor-pointer text-red-700"
+                                        icon={faTrashCan}
+                                        onClick={() =>
+                                            deleteSerialNumber(serialNumber.id)
+                                        }
+                                    />
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
             </div>
